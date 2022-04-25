315e3362e791a768892b510fe2f400ec1a07d94c
Alex Murphy <alex_murphy@flutterint.com>
2022-04-25 13:38:28 +0100

    Added tests in order to get more familiar with Swift and the test framework.

--- StopwatchUITestsGherkin/StepsAndPages/pages.swift
+++ StopwatchUITestsGherkin/StepsAndPages/pages.swift
@@ -1,6 +1,14 @@
 import Foundation
 import XCTest
 
-class Buttons {
+class Stopwatch {
     static let start = app.buttons["Start"]
+    static let stop = app.buttons["Stop"]
+    static let reset = app.buttons["Reset"]
+    static let lap = app.buttons["Lap"]
+    static let timer = app.staticTexts.element(boundBy: 1)
+    static let lapTimer = app.staticTexts.element(boundBy: 0)
+    static let lapOneTime = app.staticTexts.element(boundBy: 5)
 }
+
+
--- StopwatchUITestsGherkin/StepsAndPages/steps.swift
+++ StopwatchUITestsGherkin/StepsAndPages/steps.swift
@@ -5,25 +5,64 @@ import Nimble
 
 class NavigationSteps: StepDefiner {
     override func defineSteps() {
+        
+        step("the \"(.*)\" button is displayed") { (buttonType: String) in
+            switch buttonType{
+            case "start":
+                expect(Stopwatch.start.exists).to(beTrue())
+            case "stop":
+                expect(Stopwatch.stop.exists).to(beTrue())
+            case "lap":
+                expect(Stopwatch.lap.exists).to(beTrue())
+            case "reset":
+                expect(Stopwatch.reset.exists).to(beTrue())
+            default:
+                XCTFail("Could not find \"\(buttonType)\"")
+            }
 
-        step("the start button is displayed") {
-            XCTAssert(Buttons.start.exists == true)
         }
+        
+        step("the \"(.*)\" button is not displayed") { (buttonType: String) in
+            switch buttonType{
+            case "start":
+                expect(Stopwatch.start.exists).to(beFalse())
+            case "stop":
+                expect(Stopwatch.stop.exists).to(beFalse())
+            case "lap":
+                expect(Stopwatch.lap.exists).to(beFalse())
+            case "reset":
+                expect(Stopwatch.reset.exists).to(beFalse())
+            default:
+                XCTFail("Could not find \"\(buttonType)\"")
+            }
 
-        step("I select the start button") {
-            Buttons.start.tap()
         }
-
-        step("the stop button is displayed") {
-            XCTAssert(app.buttons["Stop"].exists == true)
+        
+        step("the timer (is|is not) set to zero"){ (displayed: String) in
+            if displayed == "is not" {
+                expect(Stopwatch.timer.label.contains("00:00:00")).to(beFalse())
+            } else {
+                    expect(Stopwatch.timer.label.contains("00:00:00")).to(beTrue())
+            }
         }
-
-        step("the start button is not displayed") {
-            XCTAssert(Buttons.start.exists == false)
+        
+        step("a time has been recorded for lap 1"){
+            expect(Stopwatch.lapOneTime.exists).to(beTrue())
         }
-
-        step("the stop button is not displayed") {
-            XCTAssert(app.buttons["Stop"].exists == false)
+        
+        step("I select the \"(.*)\" button") { (button: String) in
+            switch button{
+            case "start":
+                Stopwatch.start.tap()
+            case "stop":
+                Stopwatch.stop.tap()
+            case "lap":
+                Stopwatch.lap.tap()
+            case "reset":
+                Stopwatch.reset.tap()
+            default:
+                XCTFail("Could not select \"\(button)\"")
+            }
         }
     }
 }
